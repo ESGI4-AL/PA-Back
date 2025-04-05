d2bc0f87acd158ad04b00aa81799e4f562e2accd
3cvms3 <cvms333@yahoo.com>
2025-04-05 16:05:00 +0200

    model: user

---
+++ src/models/user.model.js
@@ -0,0 +1,70 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const bcrypt = require('bcrypt');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true //password is optional for OAuth users
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('teacher', 'student'),
+    allowNull: false
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  microsoftId: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password') && user.password) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+    }
+  },
+  timestamps: true,
+  paranoid: true //soft delete
+});
+
+//verify mdp
+User.prototype.isValidPassword = async function(password) {
+  if (!this.password) return false;
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = User;
